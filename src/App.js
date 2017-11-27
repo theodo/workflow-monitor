845696fc93cc20ed14a657d7dda5d2d13244b276
Loïc Carbonne <loicc@theodo.fr>
2017-11-27 06:46:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Monitor from './Monitor/Monitor';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Monitor />
+      </div>
+    );
+  }
+}
+
+export default App;
