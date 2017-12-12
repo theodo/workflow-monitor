ab553ce80f9b69895c4396cc30c09069ab53bfe8
yannickw <yannickw@theodo.fr>
2017-12-12 19:13:05 +0100

    Enable Redux dev tools extension

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import AppReducer from './AppReducer';
 import Monitor from './Monitor/Monitor';
 import './App.css';
 
-let store = createStore(AppReducer);
+let store = createStore(
+  AppReducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
   render() {
