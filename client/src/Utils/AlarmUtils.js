6fb2403c7c564976fb67aae4f6eb7f4a20d7027e
Loïc Carbonne <loicc@theodo.fr>
2018-10-06 00:25:59 +0100

    feat(client): remove unused call to andon light server

--- client/src/Utils/AlarmUtils.js
+++ client/src/Utils/AlarmUtils.js
@@ -1,28 +1,17 @@
-import { sendColor } from './AndonLightServer';
 
-const TIME_BEFORE_RED_LIGHT = 30000;
-
-let redLightTimer;
 let timeOverTimer;
 
 export const initAlarm = (timeInMilliSecond, withSound = true) => {
   if (timeInMilliSecond && timeInMilliSecond > 0) {
-    clearTimeout(redLightTimer);
-    sendColor('green');
     timeOverTimer = setTimeout(() => {
       if (withSound) {
         const audio = new Audio('alarm.mp3');
         audio.play();
       }
-      sendColor('orange');
-      redLightTimer = setTimeout(() => {
-        sendColor('red');
-      }, (TIME_BEFORE_RED_LIGHT));
     }, (timeInMilliSecond));
   }
 };
 
 export const cancelAlarm = () => {
   clearTimeout(timeOverTimer);
-  clearTimeout(redLightTimer);
 };
--- client/src/Utils/AndonLightServer.js
+++
@@ -1,7 +0,0 @@
-import uuid from 'uuid';
-
-export const sendColor = (color) => {
-  fetch(`https://localhost:443/${color}?refresh?${uuid()}`)
-    .then(()=>{})
-    .catch(()=>{});
-};
