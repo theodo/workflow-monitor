07165ecf82b75bf1591cc5d90dc2d1b95b88c4a4
Nicolas Boutin <nicolasb@theodo.fr>
2019-01-09 11:04:44 +0100

    refactor(client): move wrapper into index and rename dumb component into view to follow naming conventions

--- client/src/Main/Monitor/ResultPanel/index.js
+++ client/src/Main/Monitor/ResultPanel/index.js
@@ -1,1 +1,13 @@
-export { default } from './ResultPanel.wrap';
+import { connect } from 'react-redux';
+import ResultPanel from './view';
+import { setTaskFields } from '../MonitorActions';
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({
+  handleTaskChange: (taskIndex, fields) => {
+    dispatch(setTaskFields(taskIndex, fields));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ResultPanel);
