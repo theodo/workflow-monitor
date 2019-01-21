f076fc7ca7bba0c43e9341161c2656b088b4f9c5
Nicolas Boutin <nicolasb@theodo.fr>
2019-01-21 14:12:36 +0100

    WIP

--- client/src/Main/ProblemCategoryPage/view.js
+++ client/src/Main/ProblemCategoryPage/view.js
@@ -3,7 +3,9 @@ import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'Recharts';
 import AddProblemCategoryForm from './AddProblemCategoryForm';
+
 
 const fullPageHeightStyle = {
   height: '100%',
@@ -20,6 +22,14 @@ const ProblemCategoryPage = (props) => (
       <Divider variant="middle" />
       <div>
         <h3>Pareto of existing problem categories</h3>
+        <BarChart width={730} height={250} data={props.problemCategories}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="description" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="count" fill="#8884d8" />
+        </BarChart>
         <List>
           {!props.loading && props.problemCategories.map(problemCategory => (
             <ListItem key={problemCategory.id}>
