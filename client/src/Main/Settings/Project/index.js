134528315ba4c9da652f93187c43487ee290e6bd
ThomasAribart <thomasa@theodo.fr>
2019-04-25 12:26:16 +0200

    💨 Move Project component in Settings subfolder

---
+++ client/src/Main/Settings/Project/index.js
@@ -0,0 +1,28 @@
+import { initSession } from 'Main/Monitor/MonitorActions';
+import { selectProject } from 'Login/LoginActions';
+import { connect } from 'react-redux';
+
+import Project from './view';
+import { saveSettings } from '../SettingsActions';
+
+const mapStateToProps = state => {
+  return {
+    project: state.LoginReducers.currentProject
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    selectProject: project => {
+      dispatch(initSession());
+      dispatch(saveSettings({ selectedBacklogId: undefined }));
+      dispatch(selectProject(project));
+      window.location.hash = '#/';
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Project);
