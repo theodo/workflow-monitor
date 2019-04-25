0eec8aefd0b51acdf2520e6bcb9ff6ffe80ff5ba
ThomasAribart <thomasa@theodo.fr>
2019-04-25 12:21:46 +0200

    🛠 Add ui folder at root with icons and colors

---
+++ client/src/ui/Icons/index.js
@@ -0,0 +1,3 @@
+export { default as ChecklistIcon } from './ChecklistIcon';
+export { default as LineChartIcon } from './LineChartIcon';
+export { default as TrelloBoardIcon } from './TrelloBoardIcon';
---
+++ client/src/ui/index.js
@@ -0,0 +1,3 @@
+export const appColors = {
+  darkGrey: '#777777'
+};
