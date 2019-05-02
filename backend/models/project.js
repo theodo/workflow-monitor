9024115ab8ed3d8560abf3eaca87839e0f90f610
Francois Hendriks <francoishendriks96@gmail.com>
2019-05-02 11:21:44 +0200

    :sparkles: Update sequelize model an migration to add celerity and dailyDevTime

--- backend/models/project.js
+++ backend/models/project.js
@@ -1,10 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const project = sequelize.define('project', {
-    name: DataTypes.STRING,
-    thirdPartyType: DataTypes.ENUM('NONE', 'TRELLO', 'JIRA'),
-    thirdPartyId: { type: DataTypes.STRING, unique: true }
-  }, {});
+  const project = sequelize.define(
+    'project',
+    {
+      name: DataTypes.STRING,
+      thirdPartyType: DataTypes.ENUM('NONE', 'TRELLO', 'JIRA'),
+      thirdPartyId: { type: DataTypes.STRING, unique: true },
+      celerity: { type: DataTypes.INTEGER, defaultValue: 6 },
+      dailyDevelopmentTime: { type: DataTypes.INTEGER, defaultValue: 21600 }, // 6 * 3600
+    },
+    {},
+  );
   project.associate = function(models) {
     // associations can be defined here
   };
