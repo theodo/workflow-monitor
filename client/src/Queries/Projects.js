73e5c6952bba4c1fcf68cc7868110c696659b3b4
Francois Hendriks <francoishendriks96@gmail.com>
2019-05-02 17:46:14 +0200

    :ok_hand: Remove get project

--- client/src/Queries/Projects.js
+++ client/src/Queries/Projects.js
@@ -1,18 +1,5 @@
 import gql from 'graphql-tag';
 
-export const GET_CURRENT_PROJECT = gql`
-  {
-    currentProject {
-      id
-      name
-      dailyDevelopmentTime
-      celerity
-      thirdPartyType
-      thirdPartyId
-    }
-  }
-`;
-
 export const SET_PROJECT_SPEED = gql`
   mutation setCurrentProjectSpeed($projectSpeed: ProjectSpeedInput!) {
     setCurrentProjectSpeed(projectSpeed: $projectSpeed)
