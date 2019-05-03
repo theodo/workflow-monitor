ff6b4fd6b48b464d7bf294a18679eaf155351f27
Victor Lebrun <victorl@theodo.fr>
2019-05-03 10:53:46 +0200

    :card_file_box: convert dailyDevelopmentTime to milliseconds

--- backend/models/project.js
+++ backend/models/project.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
       thirdPartyType: DataTypes.ENUM('NONE', 'TRELLO', 'JIRA'),
       thirdPartyId: { type: DataTypes.STRING, unique: true },
       celerity: { type: DataTypes.INTEGER, defaultValue: 6 },
-      dailyDevelopmentTime: { type: DataTypes.INTEGER, defaultValue: 21600 }, // 6 * 3600
+      dailyDevelopmentTime: { type: DataTypes.INTEGER, defaultValue: 21600000 }, // 6 * 3600 * 1000
     },
     {},
   );
