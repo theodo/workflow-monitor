194e7a33590db3b7ffa5e405a05fcaf85655f5f3
Victor Lebrun <victorl@theodo.fr>
2019-05-06 12:15:20 +0200

    Move ticket history logic in index

--- client/src/Main/ProjectHistoryPage/TicketHistory/index.js
+++ client/src/Main/ProjectHistoryPage/TicketHistory/index.js
@@ -1,3 +1,18 @@
+import React from 'react';
 import TicketHistory from './view';
 
-export default TicketHistory;
+const getTicketTasksDevelopmentTime = tasks => {
+  return Math.round(tasks.reduce((a, b) => a + b['realTime'], 0));
+};
+
+const didTicketSucceed = ticket => {
+  if (!ticket.allocatedTime || ticket.allocatedTime === 0) return null;
+
+  return getTicketTasksDevelopmentTime(ticket.tasks) <= ticket.allocatedTime;
+};
+
+const TicketHistoryContainer = ({ ticket, ...props }) => (
+  <TicketHistory {...props} ticket={ticket} didTicketSucceed={didTicketSucceed(ticket)} />
+);
+
+export default TicketHistoryContainer;
