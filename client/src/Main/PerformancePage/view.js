6cbcb81585bf644a65e2bdd1cc7e30454c451d6d
Victor Lebrun <victorl@theodo.fr>
2019-05-08 22:26:54 +0200

    :sparkles: Add performance graph

--- client/src/Main/PerformancePage/view.js
+++ client/src/Main/PerformancePage/view.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { withStyles } from '@material-ui/core/styles';
+import { appColors } from 'ui';
 
-const PerformancePage = ({ failureHistory }) => <div>{failureHistory} </div>;
+const style = {
+  chartContainer: {
+    padding: '15px 100px',
+  },
+};
 
-export default PerformancePage;
+const PerformancePage = ({ chartInput, classes }) => {
+  const data = {
+    labels: chartInput.daysLabel,
+    datasets: [
+      {
+        backgroundColor: appColors.softRed,
+        data: chartInput.failuresCountData,
+        label: 'Tickets Failed',
+      },
+    ],
+  };
+  return (
+    <div className={classes.chartContainer}>
+      <h3>Performance Indicator</h3>
+      <Bar data={data} />
+    </div>
+  );
+};
+
+export default withStyles(style)(PerformancePage);
--- client/src/ui/index.js
+++ client/src/ui/index.js
@@ -1,4 +1,5 @@
 export const appColors = {
   darkGrey: '#777777',
   lightBlack: '#555555',
+  softRed: '#c30000',
 };
