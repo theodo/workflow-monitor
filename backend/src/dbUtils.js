1d63b60a6dbc0924b2a98e7e0ad2b9854ef6b698
Victor Lebrun <victorl@theodo.fr>
2019-05-08 15:28:26 +0200

    Use only tickets table in performance request

--- backend/src/dbUtils.js
+++ backend/src/dbUtils.js
@@ -14,29 +14,20 @@ const SELECT_PROBLEM_CATEGORY_COUNT_QUERY = `
   SELECT * from "problemCategories" LEFT OUTER JOIN
     ( SELECT problems."problemCategoryId", COUNT(problems."problemCategoryId")
       FROM tasks, tickets, problems
-      WHERE tasks."ticketId" = tickets.id AND tasks.id = problems."taskId" AND problems."problemCategoryId" > 0 AND tickets."projectId" = ?
+      WHERE tasks."ticketId" = tickets.id AND tasks.id = problems." taskId" AND problems."problemCategoryId" > 0 AND tickets."projectId" = ?
       GROUP BY problems."problemCategoryId") as "currentProjectCategoriesCount"
   ON ("problemCategories".id = "currentProjectCategoriesCount"."problemCategoryId");
 `;
 
 const SELECT_DAILY_PERFORMANCE_HISTORY_QUERY = `
-  SELECT date("ticketsTime"."createdAt") as "creationDay", count(*) AS "failedTicketsCount"
-  FROM(
-      SELECT "ticketsTasks"."id", "ticketsTasks"."createdAt", "ticketsTasks"."allocatedTime", sum("ticketsTasks"."realTime") AS "developmentTime"
-      FROM(
-          SELECT "tickets"."id", "tickets"."createdAt", "allocatedTime", "realTime"
-          FROM tickets
-          INNER JOIN tasks ON "tasks"."ticketId"="tickets"."id"
-          WHERE(
-              ("tickets"."createdAt" BETWEEN :startDate AND :endDate)
-              AND ("allocatedTime" IS NOT NULL)
-              AND "tickets"."projectId"=:projectId
-          )
-      ) AS "ticketsTasks"
-      GROUP BY "ticketsTasks"."id", "ticketsTasks"."createdAt", "ticketsTasks"."allocatedTime"
-  ) AS "ticketsTime"
-  GROUP BY date("ticketsTime"."createdAt")
-  ORDER BY "creationDay" ASC;
+  SELECT date("createdAt") as "creationDay", count(*) AS "failedTicketsCount"
+  FROM "tickets"
+  WHERE
+      "projectId"=:projectId
+      AND "allocatedTime" < "realTime"
+      AND "createdAt" BETWEEN :startDate AND :endDate
+  GROUP BY date("createdAt")
+  ORDER BY date("createdAt");
 `;
 
 module.exports = {
