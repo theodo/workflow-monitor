912848bdf62d8f1d6dbbbd9ec3cff619cf536d66
Victor Lebrun <victorl@theodo.fr>
2019-05-09 12:08:10 +0200

    :sparkles: Add new and previous week buttons in performance graph

--- client/src/Main/PerformancePage/view.js
+++ client/src/Main/PerformancePage/view.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/Button';
+import FastForwardIcon from '@material-ui/icons/FastForward';
+import FastRewindIcon from '@material-ui/icons/FastRewind';
 import { appColors } from 'ui';
 
 const style = {
   chartContainer: {
-    padding: '15px 100px',
+    padding: '5px 100px',
+  },
+  weekBrowser: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  changeWeekIcon: {
+    margin: '0 3px',
   },
 };
 
-const PerformancePage = ({ chartInput, classes }) => {
+const WeekBrowser = ({ classes, disableNextWeekButton, moveDaysForward }) => {
+  return (
+    <div className={classes.weekBrowser}>
+      <IconButton onClick={moveDaysForward(-7)}>
+        <FastRewindIcon className={classes.changeWeekIcon} />
+        <span>Previous Week</span>
+      </IconButton>
+      <IconButton disabled={disableNextWeekButton} onClick={moveDaysForward(7)}>
+        <span>Next Week</span>
+        <FastForwardIcon className={classes.changeWeekIcon} />
+      </IconButton>
+    </div>
+  );
+};
+
+const PerformancePage = ({ chartInput, classes, disableNextWeekButton, moveDaysForward }) => {
   const data = {
     labels: chartInput.daysLabel,
     datasets: [
@@ -23,7 +48,12 @@ const PerformancePage = ({ chartInput, classes }) => {
   return (
     <div className={classes.chartContainer}>
       <h3>Performance Indicator</h3>
-      <Bar data={data} />
+      <Bar data={data} height={140} />
+      <WeekBrowser
+        classes={classes}
+        disableNextWeekButton={disableNextWeekButton}
+        moveDaysForward={moveDaysForward}
+      />
     </div>
   );
 };
