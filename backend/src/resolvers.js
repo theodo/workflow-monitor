6c5c1710d0843ee606903c14b9ce4de0f2e7f58d
Francois Hendriks <francoishendriks96@gmail.com>
2019-05-10 10:17:34 +0200

    :ok_hand: Remove test config for database

--- backend/src/resolvers.js
+++
@@ -1,6 +0,0 @@
-import { mergeResolvers } from 'merge-graphql-schemas';
-
-const { ticketResolvers } = require('./tickets');
-const { userResolvers } = require('./users');
-
-module.exports = mergeResolvers([ticketResolvers, userResolvers]);
