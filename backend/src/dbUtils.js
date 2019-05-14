a1e7b490689aa7056b29a6e74e1883b1c41294d1
Francois Hendriks <francoishendriks96@gmail.com>
2019-05-14 18:13:45 +0200

    :sparkles: Add projectId to problemCategories and refacto

--- backend/src/dbUtils.js
+++ backend/src/dbUtils.js
@@ -9,17 +9,6 @@ function upsert(model, value, condition) {
     }
   });
 }
-
-const SELECT_PROBLEM_CATEGORY_COUNT_QUERY = `
-  SELECT * from "problemCategories" LEFT OUTER JOIN
-    ( SELECT problems."problemCategoryId", COUNT(problems."problemCategoryId")
-      FROM tasks, tickets, problems
-      WHERE tasks."ticketId" = tickets.id AND tasks.id = problems."taskId" AND problems."problemCategoryId" > 0 AND tickets."projectId" = ?
-      GROUP BY problems."problemCategoryId") as "currentProjectCategoriesCount"
-  ON ("problemCategories".id = "currentProjectCategoriesCount"."problemCategoryId");
-`;
-
 module.exports = {
   upsert,
-  SELECT_PROBLEM_CATEGORY_COUNT_QUERY,
 };
