c6250ea56bdfcc3dd2c04fd3640794936e4bd7d3
Francois Hendriks <francoishendriks96@gmail.com>
2019-05-15 11:21:26 +0200

    :sparkles: Modified DB request so only project categories appear

--- backend/src/problemCategories/db.js
+++ backend/src/problemCategories/db.js
@@ -1,12 +1,11 @@
 const { sequelize } = require('../../models');
 
 const SELECT_PROBLEM_CATEGORY_COUNT_QUERY = `
-  SELECT * from "problemCategories" LEFT OUTER JOIN
-    ( SELECT problems."problemCategoryId", COUNT(problems."problemCategoryId")
-      FROM tasks, tickets, problems
-      WHERE tasks."ticketId" = tickets.id AND tasks.id = problems."taskId" AND problems."problemCategoryId" > 0 AND tickets."projectId" = ?
-      GROUP BY problems."problemCategoryId") as "currentProjectCategoriesCount"
-  ON ("problemCategories".id = "currentProjectCategoriesCount"."problemCategoryId");
+SELECT "categories".id, "categories"."description", COUNT("problems"."problemCategoryId")
+FROM (SELECT * FROM "problemCategories" WHERE "problemCategories"."projectId" = :projectId) AS "categories"
+LEFT OUTER JOIN "problems"
+ON "categories".id = "problems"."problemCategoryId"
+GROUP BY "categories".id, "categories"."description"
 `;
 
 class ProblemCategoryDB {
@@ -25,7 +24,7 @@ class ProblemCategoryDB {
 
   getWithCount(projectId) {
     return this.db.query(SELECT_PROBLEM_CATEGORY_COUNT_QUERY, {
-      replacements: [projectId],
+      replacements: { projectId },
       type: this.db.QueryTypes.SELECT,
     });
   }
