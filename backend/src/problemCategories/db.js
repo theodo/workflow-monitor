20291b91e9407651ddcd4a69e73a891a54b28144
Francois Hendriks <francoishendriks96@gmail.com>
2019-05-15 12:51:05 +0200

    :sparkles: Modify query to get overTime

--- backend/src/problemCategories/db.js
+++ backend/src/problemCategories/db.js
@@ -1,11 +1,16 @@
 const { sequelize } = require('../../models');
 
 const SELECT_PROBLEM_CATEGORY_COUNT_QUERY = `
-SELECT "categories".id, "categories"."description", COUNT("problems"."problemCategoryId")
-FROM (SELECT * FROM "problemCategories" WHERE "problemCategories"."projectId" = :projectId) AS "categories"
-LEFT OUTER JOIN "problems"
-ON "categories".id = "problems"."problemCategoryId"
-GROUP BY "categories".id, "categories"."description"
+  SELECT "categories".id, "categories"."description", COUNT("problemsWithOvertime"."problemCategoryId"), SUM("problemsWithOvertime"."overtime") as "overTime"
+  FROM (SELECT * FROM "problemCategories" WHERE "problemCategories"."projectId" = :projectId) AS "categories"
+  LEFT OUTER JOIN (
+    SELECT "problems".id, "problems"."problemCategoryId", SUM(CASE WHEN "estimatedTime" < "realTime" THEN "realTime" - "estimatedTime" ELSE 0 END) AS "overtime"
+    FROM "tasks", "problems"
+    WHERE "problems"."taskId" = "tasks".id
+    GROUP BY "problems".id
+  ) as "problemsWithOvertime"
+  ON "categories".id = "problemsWithOvertime"."problemCategoryId"
+  GROUP BY "categories".id, "categories"."description"
 `;
 
 class ProblemCategoryDB {
@@ -22,7 +27,7 @@ class ProblemCategoryDB {
     return this.model.findAll({ where: { projectId } });
   }
 
-  getWithCount(projectId) {
+  getCountAndOvertime(projectId) {
     return this.db.query(SELECT_PROBLEM_CATEGORY_COUNT_QUERY, {
       replacements: { projectId },
       type: this.db.QueryTypes.SELECT,
