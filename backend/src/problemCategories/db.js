f501a8bdfad1bade522520d3e0f6f637ead09a5e
Francois Hendriks <francoishendriks96@gmail.com>
2019-05-16 11:49:23 +0200

    :ok_hand: Rename vars

--- backend/src/problemCategories/db.js
+++ backend/src/problemCategories/db.js
@@ -1,18 +1,17 @@
 const { sequelize } = require('../../models');
 
-const SELECT_PROBLEM_CATEGORY_COUNT_QUERY = `
+const SELECT_PROBLEM_CATEGORY_COUNT_AND_OVERTIME_QUERY = `
   SELECT "categories".id, "categories"."description", COUNT("problemsWithOvertime"."problemCategoryId"),
-  SUM(CASE WHEN "problemsWithOvertime"."overtime" IS NOT NULL THEN "problemsWithOvertime"."overtime" ELSE 0 END) as "overTime"
+  SUM(CASE WHEN "problemsWithOvertime"."overtime" IS NOT NULL THEN "problemsWithOvertime"."overtime" ELSE 0 END) as "overtime"
   FROM (SELECT * FROM "problemCategories" WHERE "problemCategories"."projectId" = :projectId) AS "categories"
   LEFT OUTER JOIN (
-    SELECT "problems".id, "problems"."problemCategoryId", SUM(CASE WHEN "estimatedTime" < "realTime" THEN "realTime" - "estimatedTime" ELSE 0 END) AS "overtime"
+    SELECT "problems".id, "problems"."problemCategoryId", CASE WHEN "estimatedTime" < "realTime" THEN "realTime" - "estimatedTime" ELSE 0 END AS "overtime"
     FROM "tasks", "problems"
     WHERE "problems"."taskId" = "tasks".id
-    GROUP BY "problems".id
   ) as "problemsWithOvertime"
   ON "categories".id = "problemsWithOvertime"."problemCategoryId"
   GROUP BY "categories".id, "categories"."description"
-  ORDER BY "overTime" DESC
+  ORDER BY "overtime" DESC
 `;
 
 class ProblemCategoryDB {
@@ -30,7 +29,7 @@ class ProblemCategoryDB {
   }
 
   getCountAndOvertime(projectId) {
-    return this.db.query(SELECT_PROBLEM_CATEGORY_COUNT_QUERY, {
+    return this.db.query(SELECT_PROBLEM_CATEGORY_COUNT_AND_OVERTIME_QUERY, {
       replacements: { projectId },
       type: this.db.QueryTypes.SELECT,
     });
