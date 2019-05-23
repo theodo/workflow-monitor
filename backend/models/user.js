5053b61683aa8c888f121de46166a54347baa4c3
CorentinDoue <doue.coco@hotmail.fr>
2019-05-23 11:44:02 +0200

    fix : support long tasks, problems and ticket descriptions

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user', {
-    trelloId: { type: DataTypes.STRING, unique: true },
-    fullName: { type: DataTypes.STRING, allowNull: false },
-    state: DataTypes.TEXT
-  }, {});
+  const user = sequelize.define(
+    'user',
+    {
+      trelloId: { type: DataTypes.STRING, unique: true },
+      fullName: { type: DataTypes.STRING, allowNull: false },
+      state: DataTypes.TEXT,
+    },
+    {},
+  );
   user.associate = function(models) {
-    user.belongsTo(models.project, { as: 'currentProject' })
+    user.belongsTo(models.project, { as: 'currentProject' });
   };
   return user;
 };
