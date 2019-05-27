be10c784a5572cffd3e4ed0a7f65535266814294
CorentinDoue <doue.coco@hotmail.fr>
2019-05-27 18:15:30 +0200

    refactor : corrections after review

--- client/src/Main/Settings/index.js
+++ client/src/Main/Settings/index.js
@@ -1,3 +1,39 @@
+import copy from 'copy-to-clipboard';
+import { SAVE_DEFAULT_TASKS_LIST } from 'Queries/DefaultTasksLists';
+import React from 'react';
+import { gqlClient } from 'Utils/Graphql';
+import { formatDefaultTasksList } from 'Utils/TaskUtils';
 import Settings from './view';
 
-export default Settings;
+const handleTasksChange = async (tasksListId, type, tasks) => {
+  const defaultTasksList = tasksListId
+    ? {
+        // Update List
+        id: tasksListId,
+        type: type,
+        defaultTasks: formatDefaultTasksList(tasks),
+      }
+    : {
+        // Create new List
+        type,
+        defaultTasks: formatDefaultTasksList(tasks),
+      };
+  await gqlClient.mutate({
+    mutation: SAVE_DEFAULT_TASKS_LIST,
+    variables: {
+      defaultTasksList,
+    },
+  });
+};
+
+const copyToken = () => {
+  copy(localStorage.getItem('jwt_token'));
+  alert('Token copied');
+};
+
+const props = {
+  copyToken,
+  handleTasksChange,
+};
+
+export default () => <Settings {...props} />;
--- client/src/Main/Settings/view.js
+++ client/src/Main/Settings/view.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import copy from 'copy-to-clipboard';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import { withStyles } from '@material-ui/core/styles';
-
 import Project from './Project';
 import TaskEditor from '../TaskEditor/TaskEditor';
 import './Settings.css';
-import { SAVE_DEFAULT_TASKS_LIST } from 'Queries/DefaultTasksLists';
-import { formatDefaultTasksList } from 'Utils/TaskUtils';
-import { gqlClient } from 'Utils/Graphql';
 import withDefaultTasks from 'Main/shared/WithDefaultTasks.hoc';
 import { compose } from 'redux';
 
@@ -22,32 +17,8 @@ const styles = () => ({
 });
 
 class Settings extends Component {
-  handleTasksChange = async (tasksList, tasks, type) => {
-    const defaultTasksList = tasksList
-      ? {
-          id: tasksList.id,
-          type: tasksList.type,
-          defaultTasks: formatDefaultTasksList(tasks),
-        }
-      : {
-          type,
-          defaultTasks: formatDefaultTasksList(tasks),
-        };
-    await gqlClient.mutate({
-      mutation: SAVE_DEFAULT_TASKS_LIST,
-      variables: {
-        defaultTasksList,
-      },
-    });
-  };
-
-  copyToken = () => {
-    copy(localStorage.getItem('jwt_token'));
-    alert('Token copied');
-  };
-
   render() {
-    const { classes, beginningTasksList, endTasksList } = this.props;
+    const { classes, beginningTasksList, endTasksList, handleTasksChange, copyToken } = this.props;
     return (
       <div className="Settings">
         <Grid container spacing={24}>
@@ -60,19 +31,19 @@ class Settings extends Component {
             <TaskEditor
               tasks={beginningTasksList.tasks}
               updateTasks={tasks =>
-                this.handleTasksChange(beginningTasksList.defaultTasksList, tasks, 'BEGINNING')
+                handleTasksChange(beginningTasksList.defaultTasksList.id, 'BEGINNING', tasks)
               }
             />
             <h3>End tasks</h3>
             <TaskEditor
               tasks={endTasksList.tasks}
               updateTasks={tasks =>
-                this.handleTasksChange(endTasksList.defaultTasksList, tasks, 'END')
+                handleTasksChange(endTasksList.defaultTasksList.id, 'END', tasks)
               }
             />
             <Divider variant="middle" className={classes.mv25} />
             <h2>Copy CLI token :</h2>
-            <Button variant="contained" onClick={this.copyToken}>
+            <Button variant="contained" onClick={copyToken}>
               Copy
             </Button>
           </Grid>
