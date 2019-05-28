017d48c4eaf471eddff78d43fc8ab713d948f1d3
deboraornellas <debora.ornellas1@gmail.com>
2019-05-28 17:13:10 +0200

    Correct enum names and drop enum when undoing migration

--- backend/migrations/20190528092509-add-performance-type-to-project-model.js
+++ backend/migrations/20190528092509-add-performance-type-to-project-model.js
@@ -4,13 +4,17 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
       queryInterface.addColumn('projects', 'performanceType', {
-        type: Sequelize.ENUM('CASPR_TIME', 'CELERITY_TYPE'),
-        defaultValue: 'CELERITY_TYPE',
+        type: Sequelize.ENUM('CASPR_TIME', 'CELERITY_TIME'),
+        defaultValue: 'CELERITY_TIME',
       }),
     ]);
   },
 
   down: queryInterface => {
-    return Promise.all([queryInterface.removeColumn('projects', 'performanceType')]);
+    return Promise.all([
+      queryInterface
+        .removeColumn('projects', 'performanceType')
+        .then(() => queryInterface.sequelize.query('DROP TYPE "enum_projects_performanceType";')),
+    ]);
   },
 };
