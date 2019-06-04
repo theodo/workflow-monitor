7a3c9caaca5ef54644d464236ae451a6c10909e2
deboraornellas <debora.ornellas1@gmail.com>
2019-06-04 09:49:24 +0200

    Modify unauthorized exception message

--- backend-nest/src/auth/jwt.strategy.ts
+++ backend-nest/src/auth/jwt.strategy.ts
@@ -16,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: JwtPayload) {
     const user = await this.authService.validateUser(payload);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Not authorized!');
     }
     return user;
   }
