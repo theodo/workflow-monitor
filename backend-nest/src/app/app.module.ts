4ef40d138bad4334bb6fbaa0205c38a30b09e797
deboraornellas <debora.ornellas1@gmail.com>
2019-06-04 09:48:27 +0200

    Update databaseModule import location

--- backend-nest/src/app/app.module.ts
+++ backend-nest/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [
