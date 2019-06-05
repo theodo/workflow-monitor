10fc4dd3071c30a4959f4a6f846c1a15904b0256
deboraornellas <debora.ornellas1@gmail.com>
2019-06-05 10:43:22 +0200

    Update app.module and database provider

--- backend-nest/src/app/app.module.ts
+++ backend-nest/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseModule } from '../database/database.module';
+import { TicketModule } from '../ticket/ticket.module';
+import { DefaultTaskModule } from '../defaultTask/defaultTask.module';
+import { ProblemCategoryModule } from '../problemCategory/problemCategory.module';
+import { ProjectModule } from '../project/project.module';
+import { StateModule } from '../state/state.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       typePaths: [__dirname + '/../**/*.graphql'],
       context: ({ req }) => ({ req }),
+      installSubscriptionHandlers: true,
     }),
     DatabaseModule,
     UserModule,
     AuthModule,
+    TicketModule,
+    DefaultTaskModule,
+    ProblemCategoryModule,
+    ProjectModule,
+    StateModule,
   ],
 })
 export class AppModule {}
--- backend-nest/src/database/database.provider.ts
+++ backend-nest/src/database/database.provider.ts
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize-typescript';
 import { User } from '../user/user.entity';
-import { Task } from 'src/task/task.entity';
-import { Ticket } from 'src/ticket/ticket.entity';
+import { Task } from '../task/task.entity';
+import { Ticket } from '../ticket/ticket.entity';
+import { Project } from '../project/project.entity';
+import { ProblemCategory } from '../problemCategory/problemCategory.entity';
+import { DefaultTask } from '../defaultTask/defaultTask.entity';
+import { Problem } from '../problem/problem.entity';
+import { DefaultTaskList } from 'src/defaultTaskList/defaultTaskList.entity';
 
 export const databaseProviders = [
   {
@@ -15,7 +20,16 @@ export const databaseProviders = [
         password: 'caspr',
         database: 'caspr',
       });
-      sequelize.addModels([User, Task, Ticket]);
+      sequelize.addModels([
+        User,
+        Task,
+        Ticket,
+        Project,
+        ProblemCategory,
+        DefaultTask,
+        DefaultTaskList,
+        Problem,
+      ]);
       await sequelize.sync();
       return sequelize;
     },
