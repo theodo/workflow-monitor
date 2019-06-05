d34c5f21c28af7152962235834ebbbaeb4eba5da
deboraornellas <debora.ornellas1@gmail.com>
2019-06-05 10:39:13 +0200

    Update ticket module

--- backend-nest/src/ticket/ticket.module.ts
+++ backend-nest/src/ticket/ticket.module.ts
@@ -4,10 +4,12 @@ import { TicketResolvers } from './ticket.resolver';
 import { DatabaseModule } from '../database/database.module';
 import { ticketsProvider } from './ticket.provider';
 import { PassportModule } from '@nestjs/passport';
+import { taskProvider } from '../task/task.provider';
+import { TaskService } from 'src/task/task.service';
 
 @Module({
   imports: [HttpModule, DatabaseModule, PassportModule.register({ defaultStrategy: 'jwt' })],
-  providers: [TicketResolvers, TicketService, ...ticketsProvider],
+  providers: [TicketResolvers, TicketService, TaskService, ...ticketsProvider, ...taskProvider],
   exports: [TicketResolvers, TicketService, ...ticketsProvider],
 })
 export class TicketModule {}
