e888f12d499f319dc671ba8f22149e807d155817
deboraornellas <debora.ornellas1@gmail.com>
2019-06-06 13:35:40 +0200

    Complete implementation of ticket module

--- backend-nest/src/ticket/ticket.module.ts
+++ backend-nest/src/ticket/ticket.module.ts
@@ -5,11 +5,19 @@ import { DatabaseModule } from '../database/database.module';
 import { ticketsProvider } from './ticket.provider';
 import { PassportModule } from '@nestjs/passport';
 import { taskProvider } from '../task/task.provider';
-import { TaskService } from 'src/task/task.service';
+import { TaskService } from '../task/task.service';
+import { problemsProvider } from '../problem/problem.provider';
 
 @Module({
   imports: [HttpModule, DatabaseModule, PassportModule.register({ defaultStrategy: 'jwt' })],
-  providers: [TicketResolvers, TicketService, TaskService, ...ticketsProvider, ...taskProvider],
+  providers: [
+    TicketResolvers,
+    TicketService,
+    TaskService,
+    ...ticketsProvider,
+    ...taskProvider,
+    ...problemsProvider,
+  ],
   exports: [TicketResolvers, TicketService, ...ticketsProvider],
 })
 export class TicketModule {}
--- backend-nest/src/ticket/ticket.resolver.ts
+++ backend-nest/src/ticket/ticket.resolver.ts
@@ -1,43 +1,40 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver, Mutation } from '@nestjs/graphql';
+import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
 import { GraphqlAuthGuard } from '../auth/gqlAuthguard';
 import { User as CurrentUser } from '../user/user.decorator';
 import { User } from '../user/user.entity';
 import { TicketService } from './ticket.service';
-import { Ticket } from './ticket.entity';
 
 @Resolver()
 export class TicketResolvers {
   constructor(private readonly ticketService: TicketService) {}
   @Query()
   @UseGuards(GraphqlAuthGuard)
-  tickets(@CurrentUser() user: User, pagination: { limit: any; offset: any }) {
-    // const project = user.currentProject;
-    const project = {
-      id: 1,
-    };
-    // TODO: implement after implementing project
-    // return this.ticketService.getTicketsByProject(project.id, pagination.limit, pagination.offset);
-    return 0;
+  tickets(@CurrentUser() user: User, @Args('pagination') pagination: { limit: any; offset: any }) {
+    const project = user.currentProject;
+    return this.ticketService.getTicketsByProject(project.id, pagination.limit, pagination.offset);
   }
 
   @Query()
   @UseGuards(GraphqlAuthGuard)
-  dailyPerformanceHistory(@CurrentUser() user: User, { startDate, endDate }) {
-    // const projectId = user.currentProject.id;
-    const projectId = 1;
+  dailyPerformanceHistory(
+    @CurrentUser() user: User,
+    @Args('startDate') startDate,
+    @Args('endDate') endDate,
+  ) {
+    const projectId = user.currentProject.id;
     return this.ticketService.getDailyPerformanceHistory(startDate, endDate, projectId);
   }
 
   @Query()
   @UseGuards(GraphqlAuthGuard)
-  ticket(@CurrentUser() user: User, ticketId) {
+  ticket(@Args('ticketId') ticketId) {
     return this.ticketService.getTicket(ticketId);
   }
 
   @Mutation()
   @UseGuards(GraphqlAuthGuard)
-  async saveTicket(@CurrentUser() user: User, state) {
+  async saveTicket(@CurrentUser() user: User, @Args('state') state) {
     return await this.ticketService.saveTicket(user, state);
   }
 }
