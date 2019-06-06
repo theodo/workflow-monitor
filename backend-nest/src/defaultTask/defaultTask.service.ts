38e650959c348f4cb76c8a19610677149cbcbd09
deboraornellas <debora.ornellas1@gmail.com>
2019-06-06 12:33:17 +0200

    Complete implementation of defaultTask and defaultTasksList modules

--- backend-nest/src/defaultTask/defaultTask.service.ts
+++ backend-nest/src/defaultTask/defaultTask.service.ts
@@ -1,6 +1,6 @@
 import { DefaultTask } from './defaultTask.entity';
 import { Inject, Injectable } from '@nestjs/common';
-import { DefaultTaskList } from '../defaultTaskList/defaultTaskList.entity';
+import { DefaultTasksList } from '../defaultTasksList/defaultTasksList.entity';
 import { upsert } from '../database/database.utils';
 import { User } from '../user/user.entity';
 import { Project } from '../project/project.entity';
@@ -9,12 +9,12 @@ import { Project } from '../project/project.entity';
 export class DefaultTaskService {
   constructor(
     @Inject('DefaultTaskRepository') private readonly defaultTaskRepository: typeof DefaultTask,
-    @Inject('DefaultTaskListRepository')
-    private readonly defaultTaskListRepository: typeof DefaultTaskList,
+    @Inject('DefaultTasksListRepository')
+    private readonly defaultTasksListRepository: typeof DefaultTasksList,
   ) {}
 
   async getDefaultTasksList(defaultTasksListId) {
-    return this.defaultTaskListRepository.findById(defaultTasksListId, {
+    return this.defaultTasksListRepository.findById(defaultTasksListId, {
       include: [
         {
           model: this.defaultTaskRepository,
@@ -25,8 +25,8 @@ export class DefaultTaskService {
   }
 
   async getDefaultTasksListsByProject(projectId) {
-    return this.defaultTaskListRepository.findAll({
-      where: { id: projectId },
+    return this.defaultTasksListRepository.findAll({
+      where: { projectId },
       include: [
         {
           model: this.defaultTaskRepository,
@@ -40,22 +40,35 @@ export class DefaultTaskService {
   async refreshWithTasks(defaultTasksListId, defaultTasks) {
     await this.defaultTaskRepository.destroy({ where: { defaultTasksListId } });
     defaultTasks.map(async defaultTask => {
-      await this.defaultTaskRepository.create(defaultTask);
+      const newDBTask = await this.defaultTaskRepository.create(defaultTask);
     });
   }
 
   upsert(value, condition) {
-    return upsert(this.defaultTaskListRepository, value, condition);
+    return upsert(this.defaultTasksListRepository, value, condition);
   }
 
-  async saveDefaultTasksList(defaultTasksList, user: User) {
+  async saveDefaultTasksList(defaultTasksList: DefaultTasksList, user: User): Promise<number> {
     const project = user.currentProject;
 
-    const { id, defaultTasks, ...rest } = defaultTasksList;
+    const { id, defaultTasks, type } = defaultTasksList;
 
-    const persistedDefaultTasksList = id
-      ? await this.upsert({ id, ...rest, projectId: project.id }, { id })
-      : await this.upsert({ ...rest, projectId: project.id }, {});
+    let persistedDefaultTasksList: DefaultTasksList;
+
+    persistedDefaultTasksList = await this.defaultTasksListRepository
+      .findOrCreate({
+        where: { projectId: project.id, type },
+        defaults: { type, projectId: project.id },
+        include: [
+          {
+            model: Project,
+            as: 'project',
+          },
+        ],
+      })
+      .then(([taskslist, created]) => {
+        return taskslist;
+      });
 
     const formattedDefaultTasks = defaultTasks.map(defaultTask => ({
       ...defaultTask,
