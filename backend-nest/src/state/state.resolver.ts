130a86982a00c55eb59535ba8956a24cec86dbed
deboraornellas <debora.ornellas1@gmail.com>
2019-06-07 16:52:04 +0200

    Stablish real time feature in Nest backend

--- backend-nest/src/state/state.resolver.ts
+++ backend-nest/src/state/state.resolver.ts
@@ -1,28 +1,26 @@
-import { UseGuards } from '@nestjs/common';
-import { Resolver, Mutation, Subscription, Args } from '@nestjs/graphql';
+import { UseGuards, Inject } from '@nestjs/common';
+import { Resolver, Mutation, Subscription, Args, Context } from '@nestjs/graphql';
 import { GraphqlAuthGuard } from '../auth/gqlAuthguard';
 import { User as CurrentUser } from '../user/user.decorator';
 import { User } from '../user/user.entity';
 import { PubSub } from 'graphql-subscriptions';
 
-const pubsub = new PubSub();
-
 @Resolver()
 export class StateResolver {
+  constructor(@Inject('PUB_SUB') private readonly pubsub: PubSub) {}
   @Mutation()
   @UseGuards(GraphqlAuthGuard)
   async updateCurrentState(@CurrentUser() user: User, @Args('state') state) {
     const channel = 'user#' + user.id;
-    pubsub.publish(channel, { state });
+    this.pubsub.publish(channel, { state });
     await user.set('state', state);
     await user.save();
     return 1;
   }
 
   @Subscription('state')
-  @UseGuards(GraphqlAuthGuard)
-  subscribe(@CurrentUser() user: User) {
-    const channel = 'user#' + user.id;
-    return pubsub.asyncIterator(channel);
+  subscribe(@Context('userId') userId) {
+    const channel = 'user#' + userId;
+    return this.pubsub.asyncIterator(channel);
   }
 }
