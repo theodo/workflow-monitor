0f379dc9b8191f4b1c35f872ea01edaef0540ef1
deboraornellas <debora.ornellas1@gmail.com>
2019-06-11 15:06:41 +0200

    Update dotenv config

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv-safe';
-dotenv.config({ path: __dirname + '/.env' });
+import dotenv from 'dotenv-safe';
+dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
