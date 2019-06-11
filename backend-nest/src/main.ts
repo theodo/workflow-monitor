65f44a23f8a84b476de81c21d270198e52813679
deboraornellas <debora.ornellas1@gmail.com>
2019-06-11 17:34:13 +0200

    Debug staging

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,5 +1,6 @@
-import * as dotenv from 'dotenv-safe';
-dotenv.config();
+// tslint:disable: no-var-requires
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
@@ -8,10 +9,17 @@ import { EntityNotFoundFilter } from './exception/entity-not-found.filter';
 import { QueryFailedFilter } from './exception/query-failed.filter';
 
 async function bootstrap() {
+  // tslint:disable-next-line: no-console
+  console.log('Main.ts1');
+
   const app = await NestFactory.create(AppModule);
+  // tslint:disable-next-line: no-console
+  console.log('Main.ts2');
 
   app.enableCors({ credentials: true, origin: process.env.FRONT_BASE_URL });
   app.use(cookieParser());
+  // tslint:disable-next-line: no-console
+  console.log('Main.ts3');
 
   app.useGlobalFilters(new EntityNotFoundFilter());
   app.useGlobalFilters(new QueryFailedFilter());
