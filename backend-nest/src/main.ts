3aefd790d8b5ceaddaaf66f88b9d672d3cc37a89
deboraornellas <debora.ornellas1@gmail.com>
2019-06-12 10:35:09 +0200

    Add HttpsOptions to main.ts

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -2,6 +2,7 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
+import fs from 'fs';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app/app.module';
@@ -9,18 +10,19 @@ import { EntityNotFoundFilter } from './exception/entity-not-found.filter';
 import { QueryFailedFilter } from './exception/query-failed.filter';
 
 async function bootstrap() {
-  // tslint:disable-next-line: no-console
-  console.log('Main.ts1');
+  const isDev = process.env.NODE_ENV && process.env.NODE_ENV !== 'production';
 
-  const app = await NestFactory.create(AppModule);
-
-  // tslint:disable-next-line: no-console
-  console.log('Main.ts2');
+  const app = isDev
+    ? await NestFactory.create(AppModule)
+    : await NestFactory.create(AppModule, {
+        httpsOptions: {
+          cert: fs.readFileSync('/home/ubuntu/certificates/cert.pem', 'utf8'),
+          key: fs.readFileSync('/home/ubuntu/certificates/privkey.pem', 'utf8'),
+        },
+      });
 
   app.enableCors({ credentials: true, origin: process.env.FRONT_BASE_URL });
   app.use(cookieParser());
-  // tslint:disable-next-line: no-console
-  console.log('Main.ts3');
 
   app.useGlobalFilters(new EntityNotFoundFilter());
   app.useGlobalFilters(new QueryFailedFilter());
