5619e77a33425667412b7892c916f25d640a4523
deboraornellas <debora.ornellas1@gmail.com>
2019-06-19 15:09:47 +0200

    [Électron] Do not allow user to go forward after finishing a task

--- desktop_app/src/constants.js
+++ desktop_app/src/constants.js
@@ -1,9 +1,11 @@
 const ERROR_IDS = {
-  PREVIOUS_WHEN_FIRST_TASK: 'PREVIOUS_WHEN_FIRST_TASK'
+  PREVIOUS_WHEN_FIRST_TASK: 'PREVIOUS_WHEN_FIRST_TASK',
+  NEXT_WHEN_RESULTS: 'NEXT_WHEN_RESULTS'
 }
 
 const ERROR_MESSAGES = {
-  PREVIOUS_WHEN_FIRST_TASK: 'To access your planning, please go to Caspr website'
+  PREVIOUS_WHEN_FIRST_TASK: 'To access your planning, please go to Caspr website',
+  NEXT_WHEN_RESULTS: 'You already finished your ticket'
 }
 
 module.exports = { ERROR_IDS, ERROR_MESSAGES }
