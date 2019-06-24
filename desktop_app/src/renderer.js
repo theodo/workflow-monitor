5f63bbcaa0e52c3796a16ebc2024e7fd642196ab
deboraornellas <debora.ornellas1@gmail.com>
2019-06-24 17:40:10 +0200

    Show timer in electron app

--- desktop_app/src/renderer.js
+++ desktop_app/src/renderer.js
@@ -10,84 +10,100 @@ const MONITOR_STEPS = {
   WORKFLOW: 'WORKFLOW',
   RESULTS: 'RESULTS',
 };
-
-
-
 let state = {};
-let checked = false;
 
-ipcRenderer.on('new-state', async (event, newState) => {
+ipcRenderer.on('new-state', (event, newState) => {
   state = newState;
 
   console.log(state);
   switch (state.currentStep) {
     case MONITOR_STEPS.WORKFLOW:
       const currentTask = state.tasks[state.currentTaskIndex];
+      const sessionStatus = state.dateLastPause ? 'paused' : 'running';
+      const elapsedTime = formatMilliSecondToTime(
+        getTimer(state.taskChrono, state.dateLastPause),
+      );
 
       document.getElementById('current-ticket').innerHTML =
         state.currentTrelloCard.name;
       document.getElementById('current-task').innerHTML =
         currentTask.description;
+      document.getElementById('current-time').innerHTML =
+        elapsedTime
 
-      const sessionStatus = state.dateLastPause ? 'paused' : 'running';
-      const elapsedTime = formatMilliSecondToTime(
-        getTimer(state.taskChrono, state.dateLastPause),
-      );
       const estimatedTime = formatMilliSecondToTime(currentTask.estimatedTime);
-      if (state.error && state.error.id === ERROR_IDS.PREVIOUS_WHEN_FIRST_TASK) {
-        new Notification(
-          `Cannot go backwards`,
-          {
-            body: ERROR_MESSAGES.PREVIOUS_WHEN_FIRST_TASK,
-            silent: true,
-          },
-        );
-      }
-
-      else if (state.error && state.error.id === ERROR_IDS.UNCHECKED_TASK) {
-
-        const notification = new Notification(
-          `Have you completed the checks for this task?`,
-          {
-            body: ERROR_MESSAGES.UNCHECKED_TASK,
-            silent: true,
-            data: 'https://caspr.theo.do/#/monitor'
-          },
-        );
-
-        notification.onclick = function (e) {
-          window.open(e.target.data, '_blank');
-        }
-      }
-      else if (state.taskPanelChanges) {
-        if (state.taskPanelChanges.newTasks && state.taskPanelChanges.newTasks.length > 0) {
-          const lengthNewTasksArray = state.taskPanelChanges.newTasks.length;
+      const render = false;
+      if (!state.doNotShowNotification) {
+        if (state.error && state.error.id === ERROR_IDS.PREVIOUS_WHEN_FIRST_TASK) {
           new Notification(
-            `New task added`,
+            `Cannot go backwards`,
             {
-              body: state.taskPanelChanges.newTasks[lengthNewTasksArray - 1].description,
+              body: ERROR_MESSAGES.PREVIOUS_WHEN_FIRST_TASK,
               silent: true,
             },
           );
         }
-        if (state.tasks[state.currentTaskIndex].check && state.taskPanelChanges.currentTaskCheckOK) {
-          new Notification(
-            `Check completed`,
+        else if (state.error && state.error.id === ERROR_IDS.UNCHECKED_TASK) {
+
+          const notification = new Notification(
+            `Have you completed the checks for this task?`,
             {
-              body: state.tasks[state.currentTaskIndex].check,
+              body: ERROR_MESSAGES.UNCHECKED_TASK,
+              silent: true,
+              data: 'https://caspr.theo.do/#/monitor'
+            },
+          );
+
+          notification.onclick = function (e) {
+            window.open(e.target.data, '_blank');
+          }
+        }
+        else if (state.taskPanelChanges) {
+          if (state.taskPanelChanges.newTasks && state.taskPanelChanges.newTasks.length > 0) {
+            const lengthNewTasksArray = state.taskPanelChanges.newTasks.length;
+            new Notification(
+              `New task added`,
+              {
+                body: state.taskPanelChanges.newTasks[lengthNewTasksArray - 1].description,
+                silent: true,
+              },
+            );
+          }
+          if (state.tasks[state.currentTaskIndex].check && state.taskPanelChanges.currentTaskCheckOK) {
+            new Notification(
+              `Check completed`,
+              {
+                body: state.tasks[state.currentTaskIndex].check,
+                silent: true,
+              },
+            );
+          }
+        }
+        else {
+          new Notification(
+            `Caspr ${sessionStatus} - ${elapsedTime} / ${estimatedTime}`,
+            {
+              body: state.tasks[state.currentTaskIndex].description,
               silent: true,
             },
           );
         }
       }
       else {
-        new Notification(
-          `Caspr ${sessionStatus} - ${elapsedTime} / ${estimatedTime}`,
-          {
-            body: state.tasks[state.currentTaskIndex].description,
-            silent: true,
-          },
-        );
+        shouldShowTimerNotification = false;
+        if (elapsedTime > estimatedTime && shouldShowTimerNotification) {
+          const notification = new Notification(
+            `Time over for this task`,
+            {
+              body: `Click this notification to report your problems`,
+              silent: false,
+              data: 'https://caspr.theo.do/#/monitor'
+            },
+          );
+          notification.onclick = function (e) {
+            window.open(e.target.data, '_blank');
+          }
+        }
       }
       break;
     case MONITOR_STEPS.RESULTS:
@@ -103,7 +119,7 @@ ipcRenderer.on('new-state', async (event, newState) => {
           },
         );
       }
-      else {
+      else if (!state.doNotShowNotification) {
         new Notification(state.currentTrelloCard.name, {
           body: 'Ticket done!',
           silent: true,
